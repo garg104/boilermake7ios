47837a09a3b7ae6c3bbd6d8a7e4b135e534549ec
chirayu <garg104@purdue.edu>
2020-01-25 06:37:35 -0500

    order card

---
+++ boilermake7ios/OrderDetails.swift
@@ -0,0 +1,21 @@
+//
+//  OrderDetails.swift
+//  boilermake7ios
+//
+//  Created by Chirayu Garg on 1/25/20.
+//  Copyright © 2020 boilermake7ios. All rights reserved.
+//
+
+import SwiftUI
+
+struct OrderDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderDetails()
+    }
+}
