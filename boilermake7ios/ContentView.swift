3faa9c23ba909cb3d01b12a8761321d25e29550c
chirayu <garg104@purdue.edu>
2020-01-25 01:47:20 -0500

    Initial Commit

---
+++ boilermake7ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  boilermake7ios
+//
+//  Created by Chirayu Garg on 1/25/20.
+//  Copyright © 2020 boilermake7ios. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ boilermake7iosTests/boilermake7iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  boilermake7iosTests.swift
+//  boilermake7iosTests
+//
+//  Created by Chirayu Garg on 1/25/20.
+//  Copyright © 2020 boilermake7ios. All rights reserved.
+//
+
+import XCTest
+@testable import boilermake7ios
+
+class boilermake7iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
